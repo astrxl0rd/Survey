66b161745e7bbe75e34a9de70c5f7d22c4138897
Reynaldi Rahadian <astrxl0rd>
2022-07-27 15:00:26 +0700

    adding custom styling for button

---
+++ Survey/CustomProperties/customButtonProperties.swift
@@ -0,0 +1,29 @@
+//
+//  customButtonProperties.swift
+//  Survey
+//
+//  Created by BTS.id on 27/07/22.
+//
+
+import UIKit
+
+class customButtonProperties: UIButton {
+
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        config()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        config()
+        
+    }
+    
+    func config(){
+        self.layer.cornerRadius = 10
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor.systemBlue.cgColor
+    }
+
+}
